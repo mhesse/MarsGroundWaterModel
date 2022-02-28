fcedc551cf05c198dea90817d041bfe8b6f1f0fa
Marc Hesse <mhesse@jsg.utexas.edu>
2022-02-28 15:03:41 -0600

    Add files via upload

---
+++ get_topo_contours.m
@@ -0,0 +1,18 @@
+function [topo_contours] = get_topo_contours(topo_contours,theta,phi,mars_topo,dichotomy,hellas,argyre)
+% date: Dec 9, 2021
+% author: Marc Hesse
+% description: Extracts the topographis contours of dichotomy, Hellas and
+% Argyre given the elevations specified for each one in topo_contours.hellas = comp_crater_contours(elevations_hellas,theta,phi,mars_topo,hellas);
+
+%% Dichotomy contour
+[topo_contours.dichotomy.topo,topo_contours.lowlands,topo_contours.highlands] = comp_dichotomy_contours(topo_contours.dichotomy.z,theta,phi,mars_topo,dichotomy);
+topo_contours.lowlands.z = topo_contours.dichotomy.z;
+topo_contours.highlands.z = topo_contours.dichotomy.z;
+
+%% Hellas contour
+topo_contours.hellas.topo = comp_crater_contours(topo_contours.hellas.z,theta,phi,mars_topo,hellas);
+
+%% Argyre contour
+topo_contours.argyre.topo = comp_crater_contours(topo_contours.argyre.z,theta,phi,mars_topo,argyre);
+
+
